56f6181659abb9f23e6b44e659aae404530852e7
ivankhomichkov <ivan.khomichkov@gmail.com>
2025-04-10 01:21:14 +0400

    Initial Commit

---
+++ Assets/_GAME/Scripts/IInteractable.cs
@@ -0,0 +1,8 @@
+namespace _GAME.Scripts
+{
+     public interface IInteractable
+    {
+        void InteractPrimary(FPSController player);
+        void InteractSecondary(FPSController player);
+    }
+}
